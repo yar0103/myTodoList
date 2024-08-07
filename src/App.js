83019663cf456e7d7fd7407b56acf2c343705b10
arin <yurins0103@naver.com>
2024-08-07 19:23:27 +0900

    todo input complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const TodoWriteForm = ({ newTodoTitle, setNewTodoTitle, addTodo }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="새 할일을 입력해주세요"
+        value={newTodoTitle}
+        onChange={(e) => {
+          setNewTodoTitle(e.target.value);
+        }}
+      />
+      &nbsp;
+      <button onClick={addTodo}>할 일 추가</button>
+    </div>
+  );
+};
+
+const TodoListItem = ({todo, todos, i, setTodos}) => {
+  console.log(`i : ${i}`);
+  
+  const removeTodo = () => {
+    const newTodos = todos.filter((_, _i) => i != _i);
+    setTodos(newTodos);
+  }
+  return(
+    <li>{i+1}. {todo} <button onClick={removeTodo}>del</button></li>
+  )
+}
+
+const TodoList = ({todos, setTodos}) => {
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, i) => (
+          <TodoListItem key={i} i={i} todo={todo} todos={todos} setTodos={setTodos}/>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodoTitle, setNewTodoTitle] = useState('');
+
+  const addTodo = () => {
+    //공백상태도 입력되기 때문에 검사
+    if (newTodoTitle.trim().length == 0) return;
+    setTodos([...todos, newTodoTitle.trim()]);
+    setNewTodoTitle('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoWriteForm
+        newTodoTitle={newTodoTitle}
+        setNewTodoTitle={setNewTodoTitle}
+        addTodo={addTodo}
+      />
+      <hr />
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
